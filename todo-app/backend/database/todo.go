80fd9b4c872b2f53b62fec678e594edb50a52464
takuto277 <takuto.72.soccer@gmail.com>
2025-06-01 18:25:31 +0900

    add sqlite

---
+++ todo-app/backend/database/todo.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./todo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+}

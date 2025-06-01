a671e8c33e1c8a7d3a6c467acbade9ccd44f815a
takuto277 <takuto.72.soccer@gmail.com>
2025-06-01 18:25:07 +0900

    fix model

--- todo-app/backend/models/todo.go
+++ todo-app/backend/models/todo.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Todo struct {
-	ID        uint      `json:"id"`
+	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Completed bool      `json:"completed"`
 	CreatedAt time.Time `json:"created_at"`

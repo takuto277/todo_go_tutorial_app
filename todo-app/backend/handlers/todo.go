e76b49ec5048ed14f8ae2ebec6ad1f7efeb91521
takuto277 <takuto.72.soccer@gmail.com>
2025-06-03 22:26:12 +0900

    add sql createHandler

--- todo-app/backend/handlers/todo.go
+++ todo-app/backend/handlers/todo.go
@@ -1,13 +1,34 @@
 package handlers
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"log"
 )
 
-func GetTodos(c *gin.Context) {
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./todo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to the database")
 
 }
 
-func CreateTodo(c *gin.Context) {
+func createTable() {
+	todoTable := `CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		completed BOOLEAN DEFAULT FALSE,
+		created_at DATETIME,
+		updated_at DATETIME
+	);`
 
+	_, err := DB.Exec(todoTable)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
